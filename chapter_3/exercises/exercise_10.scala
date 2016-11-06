e14c663831a5e490ef8554e2ee4f913cd92e4ada
William Kimeria <wkimeria@gmail.com>
2016-11-05 18:29:29 -0700

    Chapter 3 Exercise 12

--- chapter_3/exercises/exercise_10.scala
+++ chapter_3/exercises/exercise_10.scala
@@ -49,9 +49,7 @@ object List {
 	@annotation.tailrec
 	def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = as match {
 		case Nil => z
-		case Cons(x, xs) => {
-			foldLeft(xs,f(z,x))(f)
-		}
+		case Cons(x, xs) => foldLeft(xs,f(z,x))(f)
 	}
 
 	def length2[A](as: List[A]): Int = foldLeft(as, 0)((x,y) => 1 + x)
--- chapter_3/exercises/exercise_11.scala
+++ chapter_3/exercises/exercise_11.scala
@@ -25,9 +25,7 @@ object List {
 	@annotation.tailrec
 	def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = as match {
 		case Nil => z
-		case Cons(x, xs) => {
-			foldLeft(xs,f(z,x))(f)
-		}
+		case Cons(x, xs) => foldLeft(xs,f(z,x))(f)
 	}
 
 	/*
