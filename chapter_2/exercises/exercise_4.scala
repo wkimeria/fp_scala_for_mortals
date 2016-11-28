ae79e0f3a22331da3c4f175053e0977e952b7ec2
William Kimeria <wkimeria@gmail.com>
2016-11-28 14:13:36 -0800

    Fixing Chapter 2 Exercise 4 syntax error

--- chapter_2/exercises/exercise_4.scala
+++ chapter_2/exercises/exercise_4.scala
@@ -4,7 +4,7 @@ Note that since => associates to the right, A => (B => C) can be written as A =>
 */
 
 def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
-	a, b) => f(a)(b)
+	(a, b) => f(a)(b)
 
 	/* Can also be written as
 	(a, b) => {
