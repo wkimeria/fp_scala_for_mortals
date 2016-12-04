9bb3edc91c36c079531aa163a8154e5726a80763
William Kimeria <wkimeria@gmail.com>
2016-12-03 18:10:50 -0800

    Fixing bug in Chapter 3 Exercise 25

--- chapter_3/exercises/exercise_25.scala
+++ chapter_3/exercises/exercise_25.scala
@@ -9,19 +9,12 @@ object Tree {
 	Write a function size that counts the number of nodes (leaves and branches) in a tree.
 
 	*/
-	def size[A](tr: Tree[A]): Int = {
-		def loop[A](t: Tree[A]): Int = t match {
-			case Leaf(_) => 1
-			case  Branch(left, right) => loop(left) + loop(right)
-		}
-		val count = loop(tr)
-
-		tr match {
-			case Leaf(_) => count
-			case Branch(_,_) => count + 1
-		}
+	def size[A](tr: Tree[A]): Int = tr match{
+		case Leaf(_) => 1
+		case Branch(l, r) => 1 + size(l) + size(r)
 	}
 }
+
 /*
 tests
 */
@@ -30,7 +23,7 @@ val leftBranch = Branch(Leaf(10), Leaf(20))
 val rightBranch = Branch(Leaf(30), Leaf(40))
 val tree = Branch(leftBranch, rightBranch)
 
-assert(Tree.size(tree) == 5)
+assert(Tree.size(tree) == 7)
 assert(Tree.size(leftBranch) == 3)
 assert(Tree.size(rightBranch) == 3)
 assert(Tree.size(Leaf(10)) == 1)
